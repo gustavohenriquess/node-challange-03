86947e1182d19d976fcb675e27ec3221282cb2a2
Gustavo Henrique <gustavo._henrique@hotmail.com>
2024-02-22 12:45:44 -0300

    test: created test to create pet

---
+++ src/use-cases/create-pet.spec.ts
@@ -0,0 +1,78 @@
+import { InMemoryPetsRepository } from '@/repositories/in-memory/pets-repository'
+import { describe, it, expect, beforeEach } from 'vitest'
+import { CreatePetUseCase } from './create-pet'
+import { InMemoryPetsComplementsRepository } from '@/repositories/in-memory/pets-complements-repository'
+import { InMemoryOrgsRepository } from '@/repositories/in-memory/orgs-repository'
+
+let petRepo: InMemoryPetsRepository
+let petComplementRepo: InMemoryPetsComplementsRepository
+let orgRepo: InMemoryOrgsRepository
+let sut: CreatePetUseCase
+
+describe('Create Pet', () => {
+  beforeEach(async () => {
+    petRepo = new InMemoryPetsRepository()
+    petComplementRepo = new InMemoryPetsComplementsRepository()
+    orgRepo = new InMemoryOrgsRepository()
+    sut = new CreatePetUseCase(petRepo, petComplementRepo, orgRepo)
+
+    await orgRepo.create({
+      id: 'orgId',
+      name: 'Org',
+      email: 'email@email.com',
+      person_responsible: 'Person',
+      postal_code: '123',
+      cell_phone: '123',
+      password_hash: '123',
+      state: 'São Paulo',
+      city: 'SP',
+    })
+  })
+
+  it('should create a new pet', async () => {
+    const { pet } = await sut.execute({
+      name: 'Pet',
+      about: 'About',
+      age: 1,
+      energy: 'FOUR',
+      size: 'SMALL',
+      independence: 'MEDIUM',
+      sex: 'MALE',
+      type: 'DOG',
+      environment: 'MEDIUM',
+      orgId: 'orgId',
+    })
+
+    expect(pet.id).toEqual(expect.any(String))
+  })
+
+  it('should create a new pet with complements', async () => {
+    const { pet, complements } = await sut.execute({
+      name: 'Pet',
+      about: 'About',
+      age: 1,
+      energy: 'FOUR',
+      size: 'SMALL',
+      independence: 'MEDIUM',
+      sex: 'MALE',
+      type: 'DOG',
+      environment: 'MEDIUM',
+      orgId: 'orgId',
+      complements: [{ requirement: 'requirement' }, { photo_url: 'photo_url' }],
+    })
+
+    expect(pet.id).toEqual(expect.any(String))
+    expect(complements).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          id: expect.any(String),
+          requirement: 'requirement',
+        }),
+        expect.objectContaining({
+          id: expect.any(String),
+          photo_url: 'photo_url',
+        }),
+      ]),
+    )
+  })
+})
---
+++ src/use-cases/create-pet.ts
@@ -0,0 +1,38 @@
+import { OrgsRepository } from '@/repositories/orgs-repository'
+import { PetsComplementsRepository } from '@/repositories/pets-complements-repository'
+import { PetsRepository } from '@/repositories/pets-repository'
+import { Pet, PetComplement } from '@prisma/client'
+
+interface Complements {
+  photo_url?: string
+  requirement?: string
+}
+
+interface CreatePetRequest {
+  name: string
+  about: string
+  age: number
+  energy: 'ONE' | 'TWO' | 'THREE' | 'FOUR' | 'FIVE'
+  size: 'SMALL' | 'MEDIUM' | 'LARGE'
+  independence: 'SMALL' | 'MEDIUM' | 'LARGE'
+  sex: 'MALE' | 'FEMALE'
+  type: 'DOG' | 'CAT' | 'BIRD' | 'FISH' | 'REPTILE' | 'RODENT' | 'OTHER'
+  environment: 'SMALL' | 'MEDIUM' | 'LARGE'
+  orgId: string
+  complements?: Complements[]
+}
+
+interface CreatePetResponse {
+  pet: Pet
+  complements?: PetComplement[]
+}
+
+export class CreatePetUseCase {
+  constructor(
+    private petRepo: PetsRepository,
+    private petComplementRepo: PetsComplementsRepository,
+    private orgRepo: OrgsRepository,
+  ) {}
+
+  async execute(data: CreatePetRequest): CreatePetResponse {}
+}
