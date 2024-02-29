4016bae5e2ec7cc7c15b573684a6bfe584df3284
Gustavo Henrique <gustavo._henrique@hotmail.com>
2024-02-29 15:20:43 -0300

    feat: created create org use case

--- src/use-cases/create-org.spec.ts
+++ src/use-cases/create-org.spec.ts
@@ -14,12 +14,15 @@ describe('Create Org', () => {
   it('should create a new org', async () => {
     const { org } = await sut.execute({
       name: 'Org',
+      email: 'org@mail.com',
       person_responsible: 'Person',
-      postal_code: '123',
+      postal_code: '09070000',
       cell_phone: '123',
       password: '123',
     })
 
     expect(org.id).toEqual(expect.any(String))
+    expect(org.city).toEqual(expect.any(String))
+    expect(org.state).toEqual(expect.any(String))
   })
 })
--- src/use-cases/create-org.ts
+++ src/use-cases/create-org.ts
@@ -1,8 +1,11 @@
 import { OrgsRepository } from '@/repositories/orgs-repository'
 import { Org } from '@prisma/client'
+import { hash } from 'bcryptjs'
+import axios from 'axios'
 
 interface CreateOrgRequest {
   name: string
+  email: string
   person_responsible: string
   postal_code: string
   cell_phone: string
@@ -13,7 +16,36 @@ interface CreateOrgResponse {
   org: Org
 }
 export class CreateOrgUseCase {
-  constructor(private orgRepo: OrgsRepository) {}
+  constructor(private orgsRepo: OrgsRepository) {}
 
-  async execute(org: CreateOrgRequest): Promise<CreateOrgResponse> {}
+  async execute({
+    name,
+    email,
+    password,
+    person_responsible,
+    postal_code,
+    cell_phone,
+  }: CreateOrgRequest): Promise<CreateOrgResponse> {
+    console.log('Creating org...')
+
+    const password_hash = await hash(password, 6)
+
+    const resp = await axios.get(
+      `https://api.brasilaberto.com/v1/zipcode/${postal_code}`,
+    )
+
+    const { city, state } = resp.data.result
+    const org = await this.orgsRepo.create({
+      name,
+      email,
+      person_responsible,
+      postal_code,
+      cell_phone,
+      password_hash,
+      state,
+      city,
+    })
+
+    return { org }
+  }
 }
