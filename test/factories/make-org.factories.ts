cac0110ad92a90cec7737d81fffbd216dfbafa55
Gustavo Henrique <gustavo._henrique@hotmail.com>
2024-07-16 16:22:42 -0300

    test[factories]: created factories tests

---
+++ test/factories/make-org.factories.ts
@@ -0,0 +1,30 @@
+import { faker } from '@faker-js/faker'
+import crypto from 'node:crypto'
+
+type Overwrite = {
+  id: string
+  name: string
+  email: string
+  person_responsible: string
+  postal_code: string
+  cell_phone: string
+  password_hash: string
+  state: string
+  city: string
+}
+
+export function makeOrg(overwrite?: Overwrite) {
+  return {
+    id: overwrite?.id ?? crypto.randomUUID(),
+    name: overwrite?.name ?? faker.company.name(),
+    email: overwrite?.email ?? faker.internet.email(),
+    person_responsible:
+      overwrite?.person_responsible ?? faker.person.fullName(),
+    postal_code: overwrite?.postal_code ?? faker.location.zipCode(),
+    cell_phone: overwrite?.cell_phone ?? faker.phone.number(),
+    password_hash: overwrite?.password_hash ?? faker.internet.password(),
+    state: overwrite?.state ?? faker.location.state(),
+    city: overwrite?.city ?? faker.location.city(),
+    created_at: new Date(),
+  }
+}
