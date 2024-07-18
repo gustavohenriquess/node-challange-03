18991113d5cd354fadbcb53d5f5524da46f799c2
Gustavo Henrique <gustavo._henrique@hotmail.com>
2024-07-18 12:29:44 -0300

    fix[org]: Fixed the search with prisma to use email and not name

--- src/repositories/orgs-repository.ts
+++ src/repositories/orgs-repository.ts
@@ -3,6 +3,6 @@ import { Org, Prisma } from '@prisma/client'
 export interface OrgsRepository {
   create: (data: Prisma.OrgCreateInput) => Promise<Org>
   findByName: (name: string) => Promise<Org | null>
-  findByEmail: (name: string) => Promise<Org | null>
+  findByEmail: (email: string) => Promise<Org | null>
   findById: (id: string) => Promise<Org | null>
 }
